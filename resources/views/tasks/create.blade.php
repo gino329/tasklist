74253adbfa266f3f6b1a393c82a5e8df2be3e14b
gi-no <ec2-user@ip-172-31-0-74.us-east-2.compute.internal>
2018-06-06 07:11:09 +0000

    bootstrap design4

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,19 +9,19 @@
      
     
 
-{!! Form::model($task, ['route' => 'tasks.store']) !!}
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
-<div class="form-group">
- {!! Form::label('status', 'ステータス:') !!}
- {!! Form::text('status', null, ['class' => 'form-control']) !!}
-</div>
+     <div class="form-group">
+      {!! Form::label('status', 'ステータス:') !!}
+      {!! Form::text('status', null, ['class' => 'form-control']) !!}
+     </div>
                     
-<div class="form-group">
-{!! Form::label('content', 'タスク:') !!}
-{!! Form::text('content', null, ['class' => 'form-control']) !!}
-</div>
+     <div class="form-group">
+      {!! Form::label('content', 'タスク:') !!}
+      {!! Form::text('content', null, ['class' => 'form-control']) !!}
+      </div>
                 
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
     
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,7 +8,7 @@
  <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     
 
-{!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
+        {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
@@ -19,9 +19,9 @@
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
         
-                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+                    {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
         
-            {!! Form::close() !!}
+        {!! Form::close() !!}
  </div>
 </div>        
 @endsection
