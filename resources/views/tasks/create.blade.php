86bb1585e4e79e1db190cec169d0321d5c62b777
gi-no <ec2-user@ip-172-31-0-74.us-east-2.compute.internal>
2018-06-07 03:33:29 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,11 +5,9 @@
 <h1>Tasklist新規作成ページ</h1>
 
 <div class="row">
- <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+  <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
      
-    
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
      <div class="form-group">
       {!! Form::label('status', 'ステータス:') !!}
@@ -25,6 +23,6 @@
 
     {!! Form::close() !!}
     
- </div>
-</div>    
+ </div><!--/.col-xs>
+</div><!--/.row>
 @endsection
